9989182b57d36d4802c1f087361c88781f45ddb0
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-10-29 17:20:32 -0600

    Add HTML for splash page

--- src/index.js
+++ src/index.js
@@ -10,3 +10,4 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
