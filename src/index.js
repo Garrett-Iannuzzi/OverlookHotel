94649d2d5c114f0a3a6cf3ee748e268b62468978
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-10-31 09:17:03 -0600

    Add function to manager login

--- src/index.js
+++ src/index.js
@@ -6,12 +6,22 @@ import $ from 'jquery';
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import { userInfo } from 'os';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 
 $(document).ready(() => {
-
 })
+
+$('#submit__login--admin--js').on('click', (e) => {
+  e.preventDefault();
+  let $userName = $('#ul__dropdown--admin--js input').val()
+  let $password = $('#submit__login--admin--js').prev().val()
+    console.log($userName)
+    console.log($password)
+  checkInputValue($userName, $password)
+})
+
 
 $('.splash__btn--user').on('click', () => {
   $('.form__dropdown--user').removeClass().css('backgroundColor', 'hsl(240, 17%, 86%)');
@@ -21,6 +31,8 @@ $('.splash__btn--admin').on('click', () => {
   $('.form__dropdown--admin').removeClass().css('backgroundColor', 'hsl(240, 17%, 86%)');
 });
 
-$('.submit__login').on('load', () => {
-  
-})
+function checkInputValue(userName, password) {
+  userName === 'manager' && password === 'overlook2019' ? 
+    window.location = './admin.html' : window.location = './index.html'
+}
+
