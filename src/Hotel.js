bd1697c66d847679658d183ceba2d58eda195ec3
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2019-11-01 12:08:59 -0600

    Add fetch and set up classes

---
+++ src/Hotel.js
@@ -0,0 +1,9 @@
+class Hotel {
+  constructor(userData, roomsData, bookingsData) {
+    this.user = userData;
+    this.rooms = roomsData;
+    this.bookings = bookingsData;
+  }
+}
+
+export default Hotel;
--- src/index.js
+++ src/index.js
@@ -6,9 +6,29 @@ import $ from 'jquery';
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
-import { userInfo } from 'os';
+import Hotel from './Hotel';
+// import Admin form './admin';
+// import User form './User';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
+
+let hotel;
+// let User;
+// let Admin;
+
+Promise.all([
+  fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
+    .then(data => data.json())
+    .catch(error => console.error('NO DATA')),
+  fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
+    .then(data => data.json())
+    .catch(error => console.error('NO DATA')),
+  fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
+    .then(data => data.json())
+    .catch(error => console.error('NO DATA')),
+]).then(data => {
+  hotel = new Hotel(data[0], data[1], data[2]);
+  console.log(hotel)
+})
 
 $(document).ready(() => {
 })
